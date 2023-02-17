04384f63b06a6e2fb289770eba82e6bf1d21a71f
akashsateesha0228 <119100507+akashsateesha0228@users.noreply.github.com>
2023-02-17 14:56:04 +0530

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, I am AKASH S of SEC-I\n";
   cout << "I am in Task5 and my SRN is PES1UG20CS534\n";
-  cout << "Added new error"
+  cout << "Resolved the error";
   return 0;
 }
