492c9c643982f3d412aaa0eb74df2f1c48ca9604
akashsateesha0228 <119100507+akashsateesha0228@users.noreply.github.com>
2023-02-17 14:39:41 +0530

    Create temp.cpp

---
+++ temp.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, I am AKASH S of SEC-I\n";
+  cout << "I am in Task5 and my SRN is PES1UG20CS534\n";
+  cout << "Resolved the error";
+  return 0;
+}
